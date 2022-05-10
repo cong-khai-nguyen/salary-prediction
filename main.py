98a360df7de022de515a462d5332746853c1f359
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-05-10 15:27:58 -0500

    get the data and drop some irrelevant columns

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+
+df = pd.read_csv('data/survey_results_public.csv')
+# print(df.columns.sort_values())
+# Only get the columns that are needed for our model
+df = df[["Employment", "Country", "EdLevel", "ConvertedCompYearly", "YearsCodePro"]]
+df = df.rename({"ConvertedCompYearly" : "Salary"}, axis = 1).rename({"YearsCodePro" : "YearsOfExp"}, axis = 1)
+
+# print(df.info())
+print(df)
