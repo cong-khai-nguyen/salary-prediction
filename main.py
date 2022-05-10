9b161601983647e65bb3304d708bd3dcf2f990cb
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-05-10 15:51:39 -0500

    Only collect data with people of full time employment and drop the column

--- main.py
+++ main.py
@@ -12,4 +12,11 @@ print(df.info())
 # Drop all the rows that contains missing data and reset the index
 df = df.dropna().reset_index()
 # df = df[df["YearsOfExp"].notna()]
-print(df.isnull().sum())
+# print(df.isnull().sum())
+# print(df)
+
+# Only collect data with people of full time employment
+df = df[df["Employment"] == "Employed full-time"]
+# After that, drop the column, since we no longer need it
+df = df.drop("Employment", axis = 1)
+print(df.info())
