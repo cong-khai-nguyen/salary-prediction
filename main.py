71e2cc886dc958857854b7aff97ac3edbfad86a2
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-05-10 15:47:37 -0500

    Drop all the rows that contains missing data and reset the index

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ df = pd.read_csv('data/survey_results_public.csv')
 # print(df.columns.sort_values())
 # Only get the columns that are needed for our model
 df = df[["Employment", "Country", "EdLevel", "ConvertedCompYearly", "YearsCodePro"]]
+# Rename the columns with more intuitive naming
 df = df.rename({"ConvertedCompYearly" : "Salary", "YearsCodePro" : "YearsOfExp"}, axis = 1)
+print(df.info())
 
-# print(df.info())
-print(df)
+# Drop all the rows that contains missing data and reset the index
+df = df.dropna().reset_index()
+# df = df[df["YearsOfExp"].notna()]
+print(df.isnull().sum())
