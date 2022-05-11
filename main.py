eea36edded8d34181912be9bfba7510eb70cec1a
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-05-11 14:24:52 -0500

    Now that we see the outliers for each data, we want to exclude those with salary that is too low or too high that can skew our calculations

--- main.py
+++ main.py
@@ -44,10 +44,11 @@ df.boxplot('Salary', 'Country', ax = ax)
 plt.suptitle('Salary (US$) v Country')
 plt.title('')
 plt.ylabel('Salary')
+plt.xlabel('Country')
 plt.xticks(rotation = 90)
 plt.show()
 
-# Now that we see the outliners for each data, we want to exclude those with salary that is too low or too high that can skew our calculations
+# Now that we see the outliers for each data, we want to exclude those with salary that is too low or too high that can skew our calculations
 df = df[df['Salary'] <= 250000]
 df = df[df['Salary'] >= 10000]
 df = df[df['Country'] != 'Other']
