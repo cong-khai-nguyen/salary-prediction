5542b5ebec6c1e4c1a202add3c3d875278c7643d
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-05-14 13:47:47 -0500

    create a side to switch between explore and predict page

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-import streamlit
+import streamlit as st
 from predict_page import show_predict_page
 
+# Create a side bar where we can toggle between explore and predict page
+st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
 show_predict_page()
 
