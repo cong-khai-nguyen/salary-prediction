9e81ba86d4187ad8c7eba6daab819255fbe81939
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-05-14 12:52:54 -0500

    call to load our prediction web

--- app.py
+++ app.py
@@ -3,29 +3,3 @@ from predict_page import show_predict_page
 
 show_predict_page()
 
-countries = {
-    "United States of America",
-    "India",
-    "Germany",
-    "United Kingdom of Great Britain and Northern Ireland",
-    "Canada",
-    "France",
-    "Brazil",
-    "Spain",
-    "Netherlands",
-    "Australia",
-    "Poland",
-    "Italy",
-    "Russian Federation",
-    "Sweden",
-    "Turkey",
-    "Switzerland",
-    "Israel",
-    "Norway"
-}
-eduction = {
-    'Master’s degree',
-    'Bachelor’s degree',
-    'Post Grad',
-    'Less than a Bachelor'
-}
