4b9f9f79e60bcbf2ce21dfe71a5b786f1147b1bc
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-05-14 12:49:59 -0500

    helper method that will load the prediction page on our webapp

--- predict_page.py
+++ predict_page.py
@@ -4,15 +4,15 @@ import numpy as np
 
 def load_model():
     with open('saved_model.pickle', 'rb') as file:
-        data = pickle.load()
+        data = pickle.load(file)
     return data
 
 data = load_model()
 regressor = data['model']
 le_country = data['le_country']
-le_education = data['le_education']
+le_edu = data['le_edu']
 
 def show_predict_page():
     st.title("Software Developer Salary Prediction")
 
-    st.write(""""### We need some information to predict the salary""")
+    st.write("""### We need some information to predict the salary""")
