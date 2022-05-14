847b9d8a8d33c74c2aa0be1b3e34a88928aef368
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-05-14 12:51:40 -0500

    list all the criteria needed to make a prediction on our main prediction page

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit
+from predict_page import show_predict_page
+
+show_predict_page()
+
+countries = {
+    "United States of America",
+    "India",
+    "Germany",
+    "United Kingdom of Great Britain and Northern Ireland",
+    "Canada",
+    "France",
+    "Brazil",
+    "Spain",
+    "Netherlands",
+    "Australia",
+    "Poland",
+    "Italy",
+    "Russian Federation",
+    "Sweden",
+    "Turkey",
+    "Switzerland",
+    "Israel",
+    "Norway"
+}
+eduction = {
+    'Master’s degree',
+    'Bachelor’s degree',
+    'Post Grad',
+    'Less than a Bachelor'
+}
