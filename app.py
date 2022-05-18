ab024caf8c11f6fb51306cbcc444830b6fef628b
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-05-18 14:27:08 -0500

    final changes

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from predict_page import show_predict_page
 from explore_page import show_explore_page
 # Create a side bar where we can toggle between explore and predict page
-page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
+page = st.sidebar.selectbox("Explore Or Predict", ("Explore", "Predict"))
 if page == "Predict":
     show_predict_page()
 else:
