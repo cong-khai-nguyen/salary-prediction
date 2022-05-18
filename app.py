e7bb5224584fb6269cc69f25b30839215c8154af
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-05-18 13:48:23 -0500

    testing

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import sklearn
 import pickle
-# from predict_page import show_predict_page
-# from explore_page import show_explore_page
+from predict_page import show_predict_page
+from explore_page import show_explore_page
 # Create a side bar where we can toggle between explore and predict page
 page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
 # if page == "Predict":
